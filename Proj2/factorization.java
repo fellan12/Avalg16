95d190a38df82ffb6de57837fad93b2efea8ee6b
Fellan12 <fellan12@gmail.com>
2016-11-08 13:30:10 +0100

    Tror prime decomposition är klar

--- Proj2/factorization.java
+++ Proj2/factorization.java
@@ -5,70 +5,61 @@ import java.io.*;
 
 class factorization{
 	static BigInteger TWO = new BigInteger("2");
+    static BigInteger ONE = BigInteger.ONE;
+    static BigInteger ZERO = BigInteger.ZERO;
+    static Random random = new Random();
 	static BigInteger origin;
     
-    /** get divisor **/
     private static BigInteger rho(BigInteger N) {
-        BigInteger x1 = TWO, x2 = TWO, divisor=BigInteger.ZERO;        
-        if (N.mod(TWO) == BigInteger.ZERO) 
+        BigInteger x1 = TWO, x2 = TWO, divisor= ONE;        
+        BigInteger c  = new BigInteger("-1");           // In the original algorithm, g(x)=(x^2 - 1) mod(n)
+                                                        // Nowadays it's more common to use g(x)=(x^2 + 1) mod(n)
+
+        if (N.mod(TWO).equals(ZERO)){
             return TWO;
-        while (divisor == BigInteger.ONE) 
-        {	
-            x1 = f(x1).mod(N);
-            x2 = f(f(x2)).mod(N);
-            divisor = gcd(x1.subtract(x2).abs(), N);
-        };
-        /** return divisor **/
+        }
+
+        while((divisor.equals(ONE))){
+            x1  = f(x1,N,c);
+            x2 = f(f(x2,N,c),N,c);
+            divisor = x1.subtract(x2).gcd(N);
+        }
+
         return divisor;
     }
 
-    /** function X * X + C, change value of C as required **/
-     private static BigInteger f(BigInteger X){
-        return X.multiply(X).add(BigInteger.ONE);
-    }
-    
-    /** GCD of two numbers **/
-    private static  BigInteger gcd(BigInteger p, BigInteger q){
-        if (p.mod(q) == BigInteger.ZERO)
-            return q;
-        return gcd(q, p.mod(q));
+    private static BigInteger f(BigInteger X, BigInteger N, BigInteger C){
+        BigInteger x = X.multiply(X).mod(N);
+        x = x.add(C).mod(N);
+        return x;
     }
 
-    /** Check if num is prime **/
-    private static boolean isPrime(BigInteger N){
-        for (BigInteger i = TWO; i.intValue() <= Math.sqrt(N.intValue()); i = i.add(BigInteger.ONE))
-            if (N.mod(i) == BigInteger.ZERO)
-                return false;
-        return true;
-    }
-
-    /** get all factors **/
     private static void factor(BigInteger N){	
-        if (N == BigInteger.ONE)
+        if (N.equals(BigInteger.ONE)){
             return;
-        if (isPrime(N)) 
+        }
+
+        if (N.isProbablePrime(10))      //1 - 1/2^certainty that N is a prime
         {
-        	if(N.equals(origin)){
-        		System.out.println("fail");
-        		return;
-        	}else{
-            	System.out.println(N);
-            	return; 
-        	}
+        	System.out.println(N);
+        	return; 
         }
         BigInteger divisor = rho(N);
-        factor(divisor);
-        factor(N.divide(divisor));
+        if(divisor.equals(BigInteger.ONE)){
+            return;
+        }else{
+            factor(divisor);
+            factor(N.divide(divisor));
+        }
     }
 
-    /** Main function **/
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         while(scan.hasNext()){
         	try{
 	        	BigInteger N = scan.nextBigInteger();
 	        	origin = N;
-	        	factor (N);
+	        	factor(N);
 	        	if(scan.hasNext()){
 	        		System.out.println();
 	        	}

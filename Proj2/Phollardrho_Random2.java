0674a8acc5358166165a04f2dbcb1a97c4d6366e
Linn <bergelid@kth.se>
2016-11-30 17:09:49 +0100

    hej

--- Proj2/Phollardrho_Random2.java
+++ Proj2/Phollardrho_Random2.java
@@ -27,12 +27,12 @@ class Phollandrho_Random2{
         BigInteger c  = ONE;                                // In the original algorithm, g(x)=(x^2 - 1) mod(n)
                                                             // Nowadays it's more common to use g(x)=(x^2 + 1) mod(n)
 
-        for (BigInteger num : smallerPrimes) {
+      /*  for (BigInteger num : smallerPrimes) {
             if (N.mod(num).equals(ZERO)){                       //Get out the smaller primes
             return num;
             }
         }
-
+*/
         while((divisor.equals(ONE))){
             if(deadline.timeUntil() > 1.28*Math.pow(10,9)){    //1.28 sec remaining
                 x1  = f(x1,N,c);
--- Proj2/Phollardrho_fixed.java
+++ Proj2/Phollardrho_fixed.java
@@ -21,11 +21,11 @@ class Phollardrho_fixed{
         BigInteger divisor= ONE;
         BigInteger c  = ONE;                               
         
-        for (BigInteger num : smallerPrimes) {
+/*        for (BigInteger num : smallerPrimes) {
             if (N.mod(num).equals(ZERO)){                       //Get out the smaller primes
                return num;
            }
-        }
+        }*/
       
 
         while((divisor.equals(ONE))){
